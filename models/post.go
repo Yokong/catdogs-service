a93b8e092ea348077c7a96d5532d36fa6d44b2d4
Yoko <18836617@qq.com>
2019-06-20 21:40:19 +0800

    [add] post model

---
+++ models/post.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Post struct {
+	Id int `xorm:"pk autoincr"`
+	Title string `xorm:"varchar(120)"`
+	Content struct{}
+	Source string `xorm:"varchar(10)"`
+	Timestamp int
+	CreateTime time.Time `xorm:"created"`
+}
+
+func (p *Post) Set() error {
+	_, err := Db.Insert(p)
+	return err
+}
+
+func (p *Post) Get() (has bool, err error) {
+	has, err = Db.Get(p)
+	return has, err
+}
