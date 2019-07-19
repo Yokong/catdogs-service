f4ad8522715a60affbe4cfb0f7226a907ef51051
Yoko <18836617@qq.com>
2019-07-19 21:07:09 +0800

    [add] save post

--- services/post/setpost.go
+++ services/post/setpost.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	pb "catdogs-proto"
+	"catdogs-service/models"
 	"context"
 	"fmt"
+	"time"
 )
 
 func PosterHandler(ctx context.Context, req *pb.SetPostReq, rsp *pb.SetPostRsp) error {
 	rsp.Code = 0
 	rsp.Msg = "success"
 	fmt.Println(req.Content)
+	post := models.Post{
+		Title:     req.Title,
+		Content:   []byte(req.Content),
+		Author:    req.Author,
+		Timestamp: int(time.Now().Unix()),
+	}
+	err := post.Set()
+	if err != nil {
+		return err
+	}
 	return nil
 }
